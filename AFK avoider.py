3932a7f0b7d4d4c3e062d1a3e0c3057ce19cf754
Om Jadhav <omj82004@gmail.com>
2023-05-08 19:55:08 +0400

    made is moving function to return bool value

--- AFK avoider.py
+++ AFK avoider.py
@@ -3,9 +3,20 @@ import pyautogui as pgui
 import random
 import time
 
+def ismoving():
+    initial=pgui.position()
+    time.sleep(1)
+    final=pgui.position()
+    if initial==final:
+        return False
+    else:
+        return True
+
 #make this so it moves after checking if the mouse has moved for a certain amount of time and have a breakout function
 while True:
     x=random.randint(800,1000)
     y=random.randint(400,600)
     pgui.moveTo(x,y,0.5)#check functioning
     time.sleep(5)
+
+
