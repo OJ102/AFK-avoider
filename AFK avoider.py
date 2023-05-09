86831a8cab7526af34d5b1bb3e4a872b47256da1
Om Jadhav <omj82004@gmail.com>
2023-05-09 19:39:09 +0400

    Made a prototype pop up window function to replace the break being used currently still need to add functionality and refine it

--- AFK avoider.py
+++ AFK avoider.py
@@ -2,8 +2,9 @@
 import pyautogui as pgui
 import random
 import time
+import tkinter as tk
 
-def ismoving():
+def ismoving():#Checks if the cursor is moving
     initial=pgui.position()
     time.sleep(1)
     final=pgui.position()
@@ -15,13 +16,31 @@ def ismoving():
     else:
         return True
 
-#make this so it moves after checking if the mouse has moved for a certain amount of time and have a breakout function
-while True:
-    if ismoving():
-        break
-    x=random.randint(800,1000)
-    y=random.randint(400,600)
-    pgui.moveTo(x,y,0.5)#check functioning
+#Tkinter function to display a pop up to kill the script
+def popup():
+    root=tk.Tk()
+    root.geometry("400x200")
+    root.title("AFK avoider")
+
+    label=tk.Label(root,text="Are you back?",font=("times new roman",20))
+    label.pack(pady=20)
+
+    button1=tk.Button(root,text="Yes",font=("times new roman",20))
+    button1.pack(padx=10,pady=10)
+    button2=tk.Button(root,text="No",font=("times new roman",20))
+    button2.pack(padx=10,pady=10)
+    root.mainloop()
+    
+
+popup()
+
+
+# while True:
+#     if ismoving():#Checks if the user is moving the mouse
+#         break
+#     x=random.randint(800,1000)
+#     y=random.randint(400,600)
+#     pgui.moveTo(x,y,0.5)#check functioning
     
 
 
